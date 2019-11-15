bdc3ec329177adc3e1d61f370d3b0a86c3591af2
candy <15803469205@163.com>
2019-11-15 10:49:48 +0800

    脚手架添加react jsx模板

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,6 @@
 module.exports = [{
     name: "jsx-template",
-    value: 'git@codehub.devcloud.huaweicloud.com:2018smgxzjpt_fanxing00001/tsxcli-template.git',
+    value: 'git@github.com:yangyang158/react-jsx-template.git',
     description: "一个react项目模板",
 }, {
     name: "tsx-template",
